4baede57fff896eec0a2e24188361286743af53b
Carlos Florencio <carlosmflorencio@gmail.com>
2016-12-24 11:13:22 +0000

    Added expresso24 to index

--- index.js
+++ index.js
@@ -3,3 +3,4 @@
 module.exports.correos = require('./src/correosTracker')
 module.exports.sky = require('./src/skyTracker')
 module.exports.adicional = require('./src/adicionalTracker')
+module.exports.expresso24 = require('./src/expresso24Tracker')
