0f6041f7b8f5d9dc04b043ff9e2818dcfdd06d29
Carlos Florencio <carlosmflorencio@gmail.com>
2017-04-14 15:50:52 +0100

    Added support for Gearbest Sweden Q..XX

--- src/directLink.js
+++ src/directLink.js
@@ -9,6 +9,8 @@ const zone = "Europe/Stockholm" // +2h
 const textsURL = 'https://tracking.directlink.com/javascript/timeline_struct.js.php?lang=en'
 const URL = 'https://tracking.directlink.com/responseStatus.php?json=1&site_cd=AC3&lang=en&postal_ref_no={{id}}'
 
+const orderURL = 'https://tracking.directlink.com/multipletrack-client2.php?lang=en&postal_ref_mode=0&order_no={{order}}'
+
 const directLink = {}
 
 /**
@@ -19,7 +21,57 @@ const directLink = {}
  * @param callback(Error, DirectLinkInfo)
  */
 directLink.getInfo = function (id, callback) {
-    obtainTexts(function(err1, texts){
+    if (/Q.+XX$/.test(id)) { // order number Q24324234XX
+        // get the item number from the order number
+
+        this.getItemNumber(id, (err, item) => {
+            if(err)
+                return callback(err)
+
+            getByItemNumber(item, callback)
+        })
+    } else {
+        getByItemNumber(id, callback)
+    }
+}
+
+directLink.getItemNumber = function(id, callback) {
+    request(orderURL.replace('{{order}}', id), function (error, response, body) {
+        if (error) {
+            console.log('error:', error)
+            return callback(utils.getError('DOWN'))
+        }
+
+        if (response.statusCode != 200) {
+            console.log('response.statusCode: ', response.statusCode)
+            return callback(utils.getError('DOWN'))
+        }
+
+        if(body.indexOf('Sorry order number') !== -1) {
+            return callback(utils.getError('NO_DATA'))
+        }
+
+        try {
+            let idx = body.indexOf('ordernum')
+            let endIdx = body.indexOf('"', idx)
+
+            if(idx == -1 || endIdx == -1) {
+                return callback(utils.getError('PARSER'))
+            }
+
+            let idxStart = idx + 'ordernum'.length
+            let item = body.substr(idxStart,  endIdx - idxStart)
+
+            return callback(null, item)
+        } catch (error) {
+            return callback(utils.getError('PARSER'))
+        }
+
+    })
+}
+
+function getByItemNumber(id, callback) {
+    obtainTexts(function(err1, texts) {
         if(err1)
             return callback(err1)
 
@@ -34,7 +86,7 @@ directLink.getInfo = function (id, callback) {
             }
 
             if (body.length == 0) {
-               return callback(utils.getError('NO_DATA'))
+                return callback(utils.getError('NO_DATA'))
             }
 
             let entity = null
