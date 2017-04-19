b2192ced198be562070ea823f85cb7fc1a52f1de
Ricardo Cacheira <rcacheira@me.com>
2017-04-19 23:36:35 +0100

    added test id UR848385766YP on yanwen

--- test/yanwenTest.js
+++ test/yanwenTest.js
@@ -57,6 +57,81 @@ describe('yanwen', function () {
 
         });
 
+        it('should extract the messages from the website with success 2', function (done) {
+            const id = 'UR848385766YP'
+            yanwen.getInfo(id, (err, info) => {
+                assert.isNotNull(info)
+                assert.isNull(err)
+
+                assert.equal(info.id, 'UR848385766YP')
+                assert.equal(info.origin, 'China')
+                assert.equal(info.destiny, 'Portugal')
+                assert.equal(info.state, 'LISBOA International reception')
+                assert.deepEqual(info.states, [
+                    {
+                        "date": "2017-04-05T18:16:00+08:00",
+                        "state": "Shanghai Interchange has been opened for export"
+                    },
+                    {
+                        "date": "2017-04-13T03:47:00+08:00",
+                        "state": "LISBOA International reception"
+                    },
+                    {
+                        "date": "2017-04-13T03:47:00+08:00",
+                        "state": "葡萄牙里斯本 到达互换局"
+                    },
+                    {
+                        "date": "2017-04-03T22:22:00+08:00",
+                        "state": "Canton business international package to leave, the next stop [Canton business bulk]"
+                    },
+                    {
+                        "date": "2017-04-07T13:47:00+08:00",
+                        "state": "上海浦东 离开"
+                    },
+                    {
+                        "date": "2017-04-07T13:47:00+08:00",
+                        "state": "上海浦东 离开交航"
+                    },
+                    {
+                        "date": "2017-04-07T11:48:00+08:00",
+                        "state": "上海浦东 到达"
+                    },
+                    {
+                        "date": "2017-04-06T01:43:00+08:00",
+                        "state": "Shanghai Exchange Bureau has been export direct seal"
+                    },
+                    {
+                        "date": "2017-04-03T20:51:00+08:00",
+                        "state": "Guangshang international package has been closed"
+                    },
+                    {
+                        "date": "2017-04-03T20:08:00+08:00",
+                        "state": "Non-defined location Acceptance by the Post Services"
+                    },
+                    {
+                        "date": "2017-04-03T20:08:00+08:00",
+                        "state": "Guangshang international package has been received"
+                    },
+                    {
+                        "date": "2017-04-02T19:03:00+08:00",
+                        "state": "Shanghai Despatched from yanwen sorting center."
+                    },
+                    {
+                        "date": "2017-04-02T14:11:00+08:00",
+                        "state": "Shanghai Process completed in yanwen sorting center."
+                    },
+                    {
+                        "date": "2017-04-02T13:55:00+08:00",
+                        "state": "Yanwen Acceptance."
+                    }
+                ])
+
+                console.log(id + ' attempts: ' + info.retries + ' busy_count: ' + info.busy_count)
+                done()
+            })
+
+        });
+
         it('should extract the messages from the website with success even the ones between countries', function (done) {
             const id = 'UA846098317YP'
             yanwen.getInfo(id, (err, info) => {
