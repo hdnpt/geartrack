64065e638d6bb877f35c3e3c36393344ae021714
Carlos Florencio <carlosmflorencio@gmail.com>
2017-04-27 00:34:10 +0100

    Singpost is online again, tests added

--- test/singpostTest.js
+++ test/singpostTest.js
@@ -7,48 +7,48 @@ describe('Singpost', function() {
 
     describe('#Singpost', function() {
 
-        // it('should extract the messages from the website with success', function(done) {
-        //     const id = 'RF427233044SG'
-        //     singpost.getInfo(id, (err, info) => {
-        //         assert.isNull(err)
-        //
-        //         assert.equal(info.id, id)
-        //         assert.deepEqual(info.states, [
-        //             {
-        //                 "date": "2017-01-25T00:00:00+08:00",
-        //                 "state": "Product Delivered"
-        //             },
-        //             {
-        //                 "date": "2016-12-17T00:00:00+08:00",
-        //                 "state": "Held by Custom"
-        //             },
-        //             {
-        //                 "date": "2016-12-05T00:00:00+08:00",
-        //                 "state": "Held by Custom"
-        //             },
-        //             {
-        //                 "date": "2016-12-02T00:00:00+08:00",
-        //                 "state": "Arrival at Processing Center"
-        //             },
-        //             {
-        //                 "date": "2016-11-30T00:00:00+08:00",
-        //                 "state": "Arrival at Destination Post"
-        //             },
-        //             {
-        //                 "date": "2016-11-28T00:00:00+08:00",
-        //                 "state": "Despatched to overseas"
-        //             },
-        //             {
-        //                 "date": "2016-11-26T00:00:00+08:00",
-        //                 "state": "Received from Customer"
-        //             }
-        //         ])
-        //
-        //         console.log(id + ' attempts: ' + info.retries)
-        //         done()
-        //     })
-        //
-        // });
+        it('should extract the messages from the website with success', function(done) {
+            const id = 'RF427233044SG'
+            singpost.getInfo(id, (err, info) => {
+                assert.isNull(err)
+
+                assert.equal(info.id, id)
+                assert.deepEqual(info.states, [
+                    {
+                        "date": "2017-01-25T00:00:00+08:00",
+                        "state": "Product Delivered"
+                    },
+                    {
+                        "date": "2016-12-17T00:00:00+08:00",
+                        "state": "Held by Custom"
+                    },
+                    {
+                        "date": "2016-12-05T00:00:00+08:00",
+                        "state": "Held by Custom"
+                    },
+                    {
+                        "date": "2016-12-02T00:00:00+08:00",
+                        "state": "Arrival at Processing Center"
+                    },
+                    {
+                        "date": "2016-11-30T00:00:00+08:00",
+                        "state": "Arrival at Destination Post"
+                    },
+                    {
+                        "date": "2016-11-28T00:00:00+08:00",
+                        "state": "Despatched to overseas"
+                    },
+                    {
+                        "date": "2016-11-26T00:00:00+08:00",
+                        "state": "Received from Customer"
+                    }
+                ])
+
+                console.log(id + ' attempts: ' + info.retries)
+                done()
+            })
+
+        });
 
         it('should fail to extract', function(done) {
             const id = '423423424'
